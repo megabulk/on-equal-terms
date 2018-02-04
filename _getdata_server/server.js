5dd23ab91db5b4be25163ef459328b40428d385e
jonny <megabulk@grundig9000.com>
2018-02-04 14:53:10 +0700

    express.js now calls babel-node to generate posts.yml data from Google Drive (maybe switch to babel server?)

--- _getdata_server/server.js
+++ _getdata_server/server.js
@@ -8,10 +8,42 @@ const app = express();
 // Use v1 as prefix for all API endpoints
 app.use('/v1', router);
 
+//need to call `npm run getdata` or `babel-node getdata.js`
+
+//https://stackoverflow.com/questions/38288639/how-to-use-npm-scripts-within-javascript
+/*
+var child = spawn('./node_modules/.bin/webpack-dev-server', [
+    '--progress',
+    '--colors',
+    '<YOUR ENTRY FILE>'
+]);
+*/
+
+/*
+var child = spawn('./node_modules/babel-cli/bin/babel-node', [
+	'./getdata.js'
+]);
+*/
+
+/*
+child.stdout.on('data', function (data) {
+    process.stdout.write(data);
+});
+
+child.stderr.on('data', function (data) {
+    process.stdout.write(data);
+});
+
+child.on('exit', function (data) {
+    process.stdout.write('I\'m done!');
+});
+*/
+
 // respond with "hello world" when a GET request is made to the homepage
 // http://localhost:3001/v1/
 router.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('hello world');
+  var child = spawn('npm', ['run', 'getdata']);
 });
 
 // Launch the server on port 3001
